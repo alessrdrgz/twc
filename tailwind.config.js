38f350122627d01dc20a8d43dee3a1af62766070
alessrdrgz <alessrodrgz@gmail.com>
2022-11-28 17:44:57 +0100

    build: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/components/**/*.{js,jsx}', './src/**/*.{js,jsx}', './src/app/**/*.{js,jsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
