d9d5438368317a2038ed7dc22cf88ac18e83c985
aresmiiro <100922846+aresmiiro@users.noreply.github.com>
2022-11-30 13:05:49 +0100

    feat: expanded tailwind themes colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/components/**/*.{js,jsx}', './src/**/*.{js,jsx}', './src/app/**/*.{js,jsx}'],
+  content: [
+    './src/components/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+    './src/app/**/*.{js,jsx}'
+  ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#34363F',
+        primary: '#7E3C70',
+        secondary: '#B25371',
+        terciary: '#F2C175'
+      }
+    }
   },
   plugins: []
 }
