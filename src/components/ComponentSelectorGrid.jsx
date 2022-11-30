8d9c09c6963a5850d981885f57dd1b57e315bfb7
alessrdrgz <alessrodrgz@gmail.com>
2022-11-30 10:47:54 +0100

    feat: component selector grid component

---
+++ src/components/ComponentSelectorGrid.jsx
@@ -0,0 +1,33 @@
+
+import ButtonIcon from '@components/icons/ButtonIcon'
+import InputIcon from '@components/icons/InputIcon'
+import ComponentPlaceholder from '@components/ComponentPlaceholder'
+import { RadioGroup } from '@headlessui/react'
+
+const ComponentsPlaceholders = [
+  {
+    text: 'Button',
+    Icon: ButtonIcon,
+    value: 'button'
+  }, {
+    text: 'Input',
+    Icon: InputIcon,
+    value: 'input'
+  }
+]
+
+export default function ComponentSelectorGrid ({ value, setValue }) {
+  return (
+    <RadioGroup className="grid w-full h-full grid-cols-2 gap-4 mt-2" value={value} onChange={setValue}>
+      {
+        ComponentsPlaceholders.map((c, i) => {
+          return (
+            <RadioGroup.Option value={c.value} key={i}>
+              {({ checked }) => <ComponentPlaceholder text={c.text} Icon={c.Icon} checked={checked} />}
+            </RadioGroup.Option>
+          )
+        })
+      }
+    </RadioGroup>
+  )
+}
