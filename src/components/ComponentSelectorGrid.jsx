8f2c489b0d279fdbdc4a412113902d2ef187a2fc
alessrdrgz <alessrodrgz@gmail.com>
2022-12-04 05:39:17 +0100

    feat: added prisma and next auth adapter

--- src/components/ComponentSelectorGrid.jsx
+++ src/components/ComponentSelectorGrid.jsx
@@ -1,6 +1,5 @@
-
-import ButtonIcon from '@components/icons/ButtonIcon'
-import InputIcon from '@components/icons/InputIcon'
+import ButtonIcon from '@icons/ButtonIcon'
+import InputIcon from '@icons/InputIcon'
 import ComponentPlaceholder from '@components/ComponentPlaceholder'
 import { RadioGroup } from '@headlessui/react'
 
@@ -9,25 +8,34 @@ const ComponentsPlaceholders = [
     text: 'Button',
     Icon: ButtonIcon,
     value: 'button'
-  }, {
+  },
+  {
     text: 'Input',
     Icon: InputIcon,
     value: 'input'
   }
 ]
 
-export default function ComponentSelectorGrid ({ value, setValue }) {
+export default function ComponentSelectorGrid({ value, setValue }) {
   return (
-    <RadioGroup className="grid w-full h-full grid-cols-2 gap-4 mt-2" value={value} onChange={setValue}>
-      {
-        ComponentsPlaceholders.map((c, i) => {
-          return (
-            <RadioGroup.Option value={c.value} key={i}>
-              {({ checked }) => <ComponentPlaceholder text={c.text} Icon={c.Icon} checked={checked} />}
-            </RadioGroup.Option>
-          )
-        })
-      }
+    <RadioGroup
+      className="grid w-full h-full grid-cols-2 gap-4 mt-2"
+      value={value}
+      onChange={setValue}
+    >
+      {ComponentsPlaceholders.map((c, i) => {
+        return (
+          <RadioGroup.Option value={c.value} key={i}>
+            {({ checked }) => (
+              <ComponentPlaceholder
+                text={c.text}
+                Icon={c.Icon}
+                checked={checked}
+              />
+            )}
+          </RadioGroup.Option>
+        )
+      })}
     </RadioGroup>
   )
 }
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,23 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { prisma } from '@utils/db'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+      clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.name,
+          email: profile.email,
+          image: profile.avatar_url,
+          username: profile.login
+        }
+      }
     })
   ]
 }
