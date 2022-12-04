00315a7f126801749cd76f8bd62b21558af197c2
alessrdrgz <alessrodrgz@gmail.com>
2022-12-04 05:28:36 +0100

    feat: added spinner when editor is loading

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,13 @@
 'use client'
 
 import MonacoEditor from '@monaco-editor/react'
+import Spinner from '@components/Spinner'
 
-export default function Editor ({ content, language, onChangeContent }) {
+export default function Editor({ content, language, onChangeContent }) {
   const handleEditorMount = async (editor, monaco) => {
-    const { tailwindcssData, configureMonacoTailwindcss } = (await import('monaco-tailwindcss'))
+    const { tailwindcssData, configureMonacoTailwindcss } = await import(
+      'monaco-tailwindcss'
+    )
 
     monaco.languages.css.cssDefaults.setOptions({
       data: {
@@ -38,6 +41,11 @@ export default function Editor ({ content, language, onChangeContent }) {
           }}
           onChange={onChangeContent}
           onMount={handleEditorMount}
+          loading={
+            <div className="h-full w-full bg-[#1e1e1e] flex items-center justify-center">
+              <Spinner />
+            </div>
+          }
         />
       </div>
     </div>
@@ -46,25 +54,52 @@ export default function Editor ({ content, language, onChangeContent }) {
 
 if (typeof window !== 'undefined') {
   window.MonacoEnvironment = {
-    getWorker (_moduleId, label) {
+    getWorker(_moduleId, label) {
       switch (label) {
         case 'editorWorkerService':
-          return new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker', import.meta.url))
+          return new Worker(
+            new URL(
+              'monaco-editor/esm/vs/editor/editor.worker',
+              import.meta.url
+            )
+          )
         case 'css':
         case 'less':
         case 'scss':
-          return new Worker(new URL('monaco-editor/esm/vs/language/css/css.worker', import.meta.url))
+          return new Worker(
+            new URL(
+              'monaco-editor/esm/vs/language/css/css.worker',
+              import.meta.url
+            )
+          )
         case 'handlebars':
         case 'html':
         case 'razor':
-          return new Worker(new URL('monaco-editor/esm/vs/language/html/html.worker', import.meta.url))
+          return new Worker(
+            new URL(
+              'monaco-editor/esm/vs/language/html/html.worker',
+              import.meta.url
+            )
+          )
         case 'json':
-          return new Worker(new URL('monaco-editor/esm/vs/language/json/json.worker', import.meta.url))
+          return new Worker(
+            new URL(
+              'monaco-editor/esm/vs/language/json/json.worker',
+              import.meta.url
+            )
+          )
         case 'javascript':
         case 'typescript':
-          return new Worker(new URL('monaco-editor/esm/vs/language/typescript/ts.worker', import.meta.url))
+          return new Worker(
+            new URL(
+              'monaco-editor/esm/vs/language/typescript/ts.worker',
+              import.meta.url
+            )
+          )
         case 'tailwindcss':
-          return new Worker(new URL('monaco-tailwindcss/tailwindcss.worker', import.meta.url))
+          return new Worker(
+            new URL('monaco-tailwindcss/tailwindcss.worker', import.meta.url)
+          )
         default:
           throw new Error(`Unknown label ${label}`)
       }
---
+++ src/components/Spinner.jsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className="relative w-20 h-20 before:content-[''] bg-inherit after:content-[''] before:absolute after:absolute rounded-full before:w-full before:h-full before:bg-gradient-to-t before:from-primary before:to-secondary before:rounded-full before:animate-spin after:w-10/12 after:h-5/6 after:bg-inherit after:top-1/2 after:left-1/2 after:-translate-y-1/2 after:-translate-x-1/2 after:rounded-full" />
+  )
+}
